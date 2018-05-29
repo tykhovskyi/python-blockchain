93d28ed42751dff6184faab198b4964e04142e9d
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2018-05-29 12:22:30 +0300

    146: Adding a "Block" Class to the Blockchain

--- blockchain.py
+++ blockchain.py
@@ -17,6 +17,27 @@ blockchain = [genesis_block]
 open_transactions = []
 owner = 'Yurii'
 participants = {'Yurii'}
+data_file_path = 'tmp_data/blockchain.txt'
+
+
+def load_data():
+    with open(data_file_path, mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+
+load_data()
+
+
+def save_data():
+    with open(data_file_path, mode='w') as f:
+        f.write(str(blockchain))
+        f.write('\n')
+        f.write(str(open_transactions))
+
 
 
 def valid_proof(transactions, last_hash, proof):
@@ -120,6 +141,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -143,6 +165,7 @@ def mine_block():
         'proof': proof
     }
     blockchain.append(block)
+    save_data()
     return True
 
 
