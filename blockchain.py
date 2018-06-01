25d42ba6bded812476d485c28f225655bce5bcbe
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2018-06-01 16:16:29 +0300

    150: Adding a "Verification" Helper Class

--- blockchain.py
+++ blockchain.py
@@ -4,8 +4,9 @@ import hashlib as hl
 import pickle
 
 from block import Block
-from hash_util import hash_string_256, hash_block
+from hash_util import hash_block
 from transaction import Transaction
+from verificatin import Verification
 
 MINING_REWARD = 10
 
@@ -88,24 +89,13 @@ def save_data():
         print('Saving failed!')
 
 
-def valid_proof(transactions, last_hash, proof):
-    ordered_transactions = [
-        tx.to_ordered_dict()
-        for tx in transactions
-    ]
-    guess = (str(ordered_transactions) + str(last_hash) + str(proof)).encode()
-    guess_hash = hash_string_256(guess)
-    print(f'guess_hash: {guess_hash}')
-
-    return guess_hash[0:2] == '00'
-
-
 def proof_of_work():
     last_block = blockchain[-1]
     last_hash = hash_block(last_block)
     proof = 0
 
-    while not valid_proof(open_transactions, last_hash, proof):
+    verifier = Verification()
+    while not verifier.valid_proof(open_transactions, last_hash, proof):
         proof += 1
 
     return proof
@@ -164,28 +154,10 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def verify_chain():
-    print('  verify_chain()')
-    for (index, block) in enumerate(blockchain):
-        if index == 0:
-            continue
-        if block.previous_hash != hash_block(blockchain[index - 1]):
-            return False
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            print(f'Proof of work is invalid! - {block.proof}')
-            return False
-    return True
-
-
-def verify_transaction(transaction):
-    """ Checks whether the sender has enough coins. """
-    sender_balance = get_balance(transaction.sender)
-    return sender_balance >= transaction.amount
-
-
 def add_transaction(recipient, sender=owner, amount=1.0):
     transaction = Transaction(sender, recipient, amount)
-    if verify_transaction(transaction):
+    verifier = Verification()
+    if verifier.verify_transaction(transaction, get_balance):
         open_transactions.append(transaction)
         save_data()
         return True
@@ -225,10 +197,6 @@ def print_blockchain_elements():
         print('-' * 20)
 
 
-def verify_transactions():
-    return all([verify_transaction(tx) for tx in open_transactions])
-
-
 while True:
     print('\nPlease choose')
     print('1: Add a new transaction vallue')
@@ -239,6 +207,7 @@ while True:
     user_choice = get_user_choice()
     print('')
 
+    verifier = Verification()
     if user_choice == '1':
         tx_data = get_transaction_value()
         recipient, amount = tx_data
@@ -254,7 +223,7 @@ while True:
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
-        if verify_transactions():
+        if verifier.verify_transactions(open_transactions, get_balance):
             print('All transactions are valid')
         else:
             print('There are invalid transactions!')
@@ -263,7 +232,7 @@ while True:
     else:
         print('Input was invalid, please pick a value from the list!')
 
-    if not verify_chain():
+    if not verifier.verify_chain(blockchain):
         print('Invalid blockchain!')
         break
     print('Balance of {}: {:6.2f}'.format(owner, get_balance('Yurii')))
---
+++ verificatin.py
@@ -0,0 +1,48 @@
+from hash_util import hash_string_256, hash_block
+
+
+class Verification:
+
+    def valid_proof(self, transactions, last_hash, proof):
+        """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
+
+        Arguments:
+            :transactions: The transactions of the block for which the proof is created.
+            :last_hash: The previous block's hash which will be stored in the current block.
+            :proof: The proof number we're testing.
+        """
+        ordered_transactions = [
+            tx.to_ordered_dict()
+            for tx in transactions
+        ]
+        guess = (str(ordered_transactions) + str(last_hash) + str(proof)).encode()
+        guess_hash = hash_string_256(guess)
+        print(f'guess_hash: {guess_hash}')
+
+        return guess_hash[0:2] == '00'
+    
+    def verify_chain(self, blockchain):
+        """ Verify the current blockchein and return True if it's valid. """
+        print('  verify_chain()')
+        for (index, block) in enumerate(blockchain):
+            if index == 0:
+                continue
+            if block.previous_hash != hash_block(blockchain[index - 1]):
+                return False
+            if not self.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+                print(f'Proof of work is invalid! - {block.proof}')
+                return False
+        return True
+
+
+    def verify_transaction(self, transaction, get_balance):
+        """ Checks whether the sender has enough coins. """
+        sender_balance = get_balance(transaction.sender)
+        return sender_balance >= transaction.amount
+    
+    def verify_transactions(self, open_transactions, get_balance):
+        """ Verifies all open transactions. """
+        return all([
+            self.verify_transaction(tx, get_balance) 
+            for tx in open_transactions
+        ])
